039a0e63c44913c3a0f86bddea5f81ec9693df3e
Sean Kyer <sean.actor@gmail.com>
2021-03-26 16:32:54 -0700

    GUI: build html and added test javascript file for inputs

---
+++ javascript/test.js
@@ -0,0 +1,8 @@
+function patternTest() {
+    var inputText = document.getElementById("pattern").value;
+    var repetition = document.getElementById("repetitions").value.toString();
+
+    var outString = inputText.repeat(repetition);
+
+    document.getElementById("output").innerHTML = outString;
+}
