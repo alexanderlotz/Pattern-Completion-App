58252b66838921a4ca721393f9fc64f605942062
Sean Kyer <sean.actor@gmail.com>
2021-03-26 22:52:45 -0700

    Added list adding function to input.js, added multiple entry capability to index.html

---
+++ javascript/input.js
@@ -0,0 +1,51 @@
+var INPUTS = [];
+
+function addInput() {
+    var inputText = document.getElementById("pattern").value;
+    if (inputText == "") {
+        alert("Empty string detected!")
+        return
+    }
+
+    document.getElementById("pattern").value = "";
+
+    INPUTS.push(inputText);
+    console.log(INPUTS.length);
+
+    var output = "";
+
+    for (i = 0; i < INPUTS.length; i++) {
+        if (output == ""){
+            output = INPUTS[i];
+        } else {
+            output = output + ", " + INPUTS[i];
+        }
+    }
+    document.getElementById("entry").innerHTML = output;
+}
+
+function patternTest() {
+    var inputText = "";
+    for (i = 0; i < INPUTS.length; i++) {
+        inputText = inputText + INPUTS[i];
+    }
+
+    var repetition = document.getElementById("repetitions").value.toString();
+    var outString = "";
+
+    for (i = 0; i < INPUTS.length; i++) {
+        if (outString == ""){
+            outString = INPUTS[i];
+        } else {
+            outString = outString + ", " + INPUTS[i];
+        }
+    }
+
+    var temp = "";
+    for (i = 0; i < repetition; i++) {
+        temp = outString + " " + temp
+    }
+
+    document.getElementById("success-alert").innerHTML = "Output:";
+    document.getElementById("output").innerHTML = temp;
+}
